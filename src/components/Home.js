2fd79fe219f7ab2d9220177cd65db68d1e7684e5
John Nguyen <john.code9592@gmail.com>
2019-06-24 21:07:36 +1000

    load more button now loads 3 extra posts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getPosts, deletePost } from '../actions'
 
 const Home = ({ getPosts, deletePost, allPosts }) => {
+    const [currentLoad, setCurrentLoad] = useState(3)
+    const [amountToLoad] = useState(3)
+    const [nextLoad, setNextLoad] = useState(currentLoad + amountToLoad)
+    
+    const [loadAllPosts, setLoadAllPosts] = useState([])
 
     useEffect(() => {
         fetchData()
     }, [])
 
     const fetchData = async () => {
-        let request = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        let request = await fetch('https://jsonplaceholder.typicode.com/posts')
         let data = await request.json()
         getPosts(data)
     }
@@ -19,13 +24,39 @@ const Home = ({ getPosts, deletePost, allPosts }) => {
         deletePost(post)
     }
 
-    const renderData = () => {
+    const loadClick = () => {
+        let loadNextPosts = allPosts.filter(post => post.id > currentLoad && post.id <= nextLoad).map(post => {
+            return (
+                <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <Link to={`/${post.id}`}><span className="card-title">{post.title}</span></Link>
+                        <p style={{fontStyle: "italic", textDecoration: 'underline'}}>blog {post.id}</p>
+                        <p>{post.body}</p>
+                        <button
+                            onClick={() => deleteClick(post)} 
+                            style={{marginTop: "10px"}} 
+                            className="btn waves-effect waves-light red"
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            )
+        })
+        setLoadAllPosts([...loadAllPosts, ...loadNextPosts])
+    
+        setCurrentLoad(currentLoad + amountToLoad)
+        setNextLoad(nextLoad + amountToLoad)
+    }
+
+    const renderList = () => {
         if(allPosts) {
-            return allPosts.map(post => {
+            return allPosts.filter(post => post.id <= 3).map(post => {
                 return (
                     <div className="post card" key={post.id}>
                         <div className="card-content">
                             <Link to={`/${post.id}`}><span className="card-title">{post.title}</span></Link>
+                            <p style={{fontStyle: "italic", textDecoration: 'underline'}}>blog {post.id}</p>
                             <p>{post.body}</p>
                             <button
                                 onClick={() => deleteClick(post)} 
@@ -42,16 +73,22 @@ const Home = ({ getPosts, deletePost, allPosts }) => {
         return <div className="center">Loading...</div>
     }
 
+    const renderLoadButton = () => {
+        if(allPosts) {
+            return <button onClick={loadClick} style={{display: 'block', margin: 'auto'}} className="btn waves-effect waves-light">
+                Load more
+            </button>
+        }
+        return null
+    }
+
     return (
         <div className="container">
             <h4 className="center">Home</h4>
-            {renderData()}
-            <button 
-                style={{display: 'block',margin: 'auto'}} 
-                className="btn waves-effect waves-light"
-            >
-                Load more
-            </button>
+            {renderList()}
+
+            {loadAllPosts}
+            {renderLoadButton()}
         </div>
     )
 }
